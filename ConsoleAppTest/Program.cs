15c4880c20d57c9005059bb8c75e06af441840c9
Vivek <vivek@nodesautomations.com>
2020-09-10 17:28:37 +0530

    Test Project Added

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppTest
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
